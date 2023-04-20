83f31ac7d09de19ae2b82c7fd7483e2ebee9afa5
Wesley Rafael <rafaelwesley707@gmail.com>
2023-04-20 11:42:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <form id="enviar" action="media.php" method="POST">
+            nota1: <input type="number" name="nota1">
+            <br>
+            nota2: <input type="number" name="nota2">
+            <br>
+            nota3: <input type="number" name="nota3">
+            <br>
+            <input type="submit" name="enviar" value="enviar">
+        </form>
+    </center>
+</body>
+</html>
