66f6410a1bbd337d4ecd08c5cf1ecd53ea39089e
Wesley Rafael <rafaelwesley707@gmail.com>
2023-05-25 11:36:01 -0300

    Add files via upload

---
+++ wesley.r/wesleyy/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center> <h1>
+
+    <form id = "login" action = "logado.php" method = "POST">
+    Login: <input type = "text" name = "login" required><br>
+    Senha: <input type = "passeword" name = "senha" required>
+    <br>
+    <input type = "submit" name = entrar value = "entrar">
+    <br>
+
+</center>
+</form>
+</body>
+</html>
