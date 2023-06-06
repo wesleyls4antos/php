9cc7dc16a03134e6ab1363c375d02b30651f0553
Wesley Rafael <rafaelwesley707@gmail.com>
2023-06-06 11:37:58 -0300

    Add files via upload

---
+++ wesley.r/cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "estile.css">
+    <title>Document</title>
+</head>
+<body>
+<center>
+        <h1><br><br><br><br><br><br>
+            <form id="login" action="cadastrar.php" method="POST">
+                Nome: <input type="text" name="nome" required><br> 
+                Login: <input type="text" name="login" required><br>
+                Senha: <input type="password" name = "senha" required><br>
+                <input type = "submit" name = "cadastrar" value = "Cadastrar">
+            </form>
+        </h1>
+    </center>
+</body>
+</html>
---
+++ wesley.r/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "estile.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<center> <h1><br><br><br><br><br><br><br>
+    <form id = "login" action = "logado.php" method = "POST">
+    Login: <input type = "text" name = "login"> 
+    <br>
+    Senha: <input type = "password" name = "senha">
+    <br>
+    <input type = "submit" name = entrar value =  "entrar">
+    <br>
+
+</center>
+</form>
+</body>
+</html>
